fbf9f9b7f5ca84b05278968516367a3d8f84c8c6
arbab57 <165644659+arbab57@users.noreply.github.com>
2024-07-29 18:30:29 +0500

    idk

--- server.js
+++ server.js
@@ -9,26 +9,31 @@ const {
 const port = 8000;
 
 const server = http.createServer((req, res) => {
+  // get all
   if (req.url === "/api/products" && req.method === "GET") {
     getProducts(req, res);
-  } else if (
-    req.url.match(/\/api\/product\/([0-9]+)/) &&
-    req.method === "GET"
-  ) {
+  }
+  // get single product
+  else if (req.url.match(/\/api\/product\/([0-9]+)/) && req.method === "GET") {
     getSingleProduct(req, res);
-  } else if (
-    req.url.match(/\/api\/product\/([0-9]+)/) &&
-    req.method === "PUT"
-  ) {
+  }
+  // update product
+  else if (req.url.match(/\/api\/product\/([0-9]+)/) && req.method === "PUT") {
     updateProduct(req, res);
-  } else if (
+  }
+  // delete product
+  else if (
     req.url.match(/\/api\/product\/([0-9]+)/) &&
     req.method === "DELETE"
   ) {
     deleteProduct(req, res);
-  } else if (req.url === "/api/products" && req.method === "POST") {
+  }
+  //  create product
+  else if (req.url === "/api/products" && req.method === "POST") {
     createProduct(req, res);
-  } else {
+  }
+  //all other routes
+  else {
     res.writeHead(404, { "Content-Type": "text/plain" });
     res.write("Route not Found");
     res.end();
